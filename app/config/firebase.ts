682ae9c8a61de20c3a8d89f999fbdc130b62d05e
Ali Ibrahim Al-Sanabani <pro.sanabani@gmail.com>
2024-08-29 12:41:47 +0300

    Update firebase.ts

--- app/config/firebase.ts
+++ app/config/firebase.ts
@@ -1,15 +1,21 @@
-import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: 'AIzaSyCtIxnfhsdAtsISpIbIf47hAne0VbBvqDg',
-  appId: '1:964587286081:web:66b2331e1028b795bfe0c0',
-  authDomain: 'oems-93b28.firebaseapp.com',
-  measurementId: 'G-5D4HRWMKQY',
-  messagingSenderId: '964587286081',
-  projectId: 'oems-93b28',
-  storageBucket: 'oems-93b28.appspot.com',
+  apiKey: "AIzaSyCtIxnfhsdAtsISpIbIf47hAne0VbBvqDg",
+  authDomain: "oems-93b28.firebaseapp.com",
+  projectId: "oems-93b28",
+  storageBucket: "oems-93b28.appspot.com",
+  messagingSenderId: "964587286081",
+  appId: "1:964587286081:web:66b2331e1028b795bfe0c0",
+  measurementId: "G-5D4HRWMKQY"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const FirebaseDatabase = getFirestore(app);
